a5e7ff51e2196ccf8aad543a87517f4952d6a8ec
Himanshu <himanshu.chhikara@oimedia.in>
2020-11-03 20:50:05 +0530

    3-nov

---
+++ DS algo 2/code.js
@@ -0,0 +1,29 @@
+
+var inputData = [
+    {
+        id: 1,
+        title: 'hippo',
+        faveFood: 'carrots'
+    },
+    {
+        id: 2,
+        title: 'Cat',
+        faveFood: 'carrots'
+    },
+    {
+        id: 3,
+        title: 'ducks',
+        faveFood: 'breadcrumbs'
+    },
+]
+
+findAnimal = function() {
+    for(x=0; x<3; x++) {
+        if(inputData[x].title = args[0]) {
+            var answer = inputData[x].faveFood
+        }
+    }
+    return answer
+}
+
+console.log(findAnimal('hippo'))
