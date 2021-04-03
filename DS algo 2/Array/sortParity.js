ef55b944b4a6ec4f076dc1ebdcf8baaae852c540
Himanshu <himanshu.chhikara@oimedia.in>
2021-04-03 16:34:59 +0530

    sort by parrity 2

--- DS algo 2/Array/sortParity.js
+++ DS algo 2/Array/sortParity.js
@@ -1,15 +1,19 @@
 var sortArrayByParity = function(A) {
-      var output = []
+      // var output = []
       var len = A.length;
-
+      var index = 0;
       for(var i=0;i<len;i++){
-          if(A[i] % 2 == 0){
-            output.unshift(A[i]);
-          }
-          else
-            output.push(A[i])
+        // console.log(i);
+         if( A[i] % 2 == 0){
+           console.log("Value of i " + i)
+           console.log(A[index])
+           //Here Swapping of even number is done
+          var temp = A[index];
+          A[index++] = A[i];
+          A[i] = temp
+         }
       }
-      return output
+      return A
 };
 
 console.log(sortArrayByParity([3,1,2,4]))
--- clousreCount.js
+++ clousreCount.js
@@ -2,7 +2,7 @@ function log(){
     var count =0;
 
     return function(){
-        console.log("count before increment =="+count);
+        // console.log("count before increment =="+count);
         count += 1;
         return count
         console.log(count);
